eea6347778f01a9c9baf4cf4111d174a02edcbad
Florian Oswald <florian.oswald@gmail.com>
2013-03-15 21:28:57 +0000

    changed knot selector

--- R/F_SplineInt1D.r
+++ R/F_SplineInt1D.r
@@ -9,8 +9,10 @@ require(Matrix)
 #' @family frep
 #' @example examples/F_SplineInt1D_example.r
 F_SplineInt1D <- function(xsupp,ivals) {
-  xknots = splineKnotsMpec(xsupp)$knots
-  Nx     = splineKnotsMpec(xsupp)$N
+	#   xknots = splineKnotsMpec(xsupp)$knots
+	#   Nx     = splineKnotsMpec(xsupp)$N
+  xknots = knot.select(grid=xsupp,deg=3)	# selects knots such that num of spline coefs == num data points, i.e. there is an exact solution.
+  Nx     = length(xknots)
 
  ff <- function(ain,zin,gin,deriv=0) {
 
