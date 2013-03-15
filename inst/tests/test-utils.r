8cbadadf5ced6bf74b867c226d2e95c2834aa071
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2013-03-15 22:48:47 +0000

    test case for ipoptr.sparse

---
+++ inst/tests/test-utils.r
@@ -0,0 +1,15 @@
+library('testthat')
+library('mpeccable')
+library('ipopt')
+context("FDiff")
+
+test_that("Check that ipoptr.sparse is correct", {
+
+
+    M = Matrix(  pmax(runif(30^2)-0.8,0) ,30,30,sparse=TRUE)
+    M1 = make.sparse(M)
+    M2 = ipoptr.sparse(M)
+
+    expect_true(all(unlist(M1) == unlist(M2)))
+} )
+
